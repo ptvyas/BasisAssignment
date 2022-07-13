33f62896f0d09be79468d57aa3c3bfd253942102
Lokesh <vyasbhagat@gmail.com>
2022-07-13 11:00:05 +0530

    Create Project

---
+++ BasisPreticalTask/BasisPreticalTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BasisPreticalTask
+//
+//  Created by Vyas on 13/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
